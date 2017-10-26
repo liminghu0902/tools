fb321d846714addfe19fd6ee1f1701f3368c8e2c
李明虎 <3173329910@qq.com>
2017-10-26 14:38:29 +0800

    add ImageLoader

---
+++ src/imageLoader/index.js
@@ -0,0 +1,135 @@
+
+function ImageLoader(options) {
+
+	var self = this;
+
+	if(!options || !options.images || !(options.images instanceof Array) || options.images.length === 0) {
+		return false;
+	}
+
+	self.opts = options;
+
+	//图片数组
+	self.images = options.images;
+
+	//默认所有图片加载成功
+	self.success = true;
+
+	//是否超时
+	self.isTimeout = false;
+
+	//加载图片的数量
+	self.count = 0;
+
+	var images = self.images;
+
+	for(var key in images) {
+
+		//过滤原型链上的属性
+		if(!images.hasOwnProperty(key)) {
+			continue;
+		};
+		
+		var item = images[key];
+
+		//如果item是字符串类型，则转换为Object
+		if(typeof item === 'string') {
+			item = images[key] = {
+				src: item
+			}
+		};
+
+		if(!item || !images[key]) {
+			continue;
+		};
+
+		item.img = new Image();
+
+		setTimeout(function(){
+			self.onLoad(item)
+		}, 2000)
+		// self.onLoad(item);
+	}
+
+	if(self.opts.timeout) {
+		self.onTimeout();
+	}
+
+}
+
+//开始加载图片
+ImageLoader.prototype.onLoad = function(item) {
+	var self = this;
+	
+	if(!item) {
+		return false;
+	}
+
+	item.img.onload = function() {
+		self.success = self.success && true;
+		item.success = true;
+		self.done(item)
+	};
+	item.img.onerror = function() {
+		self.success = item.success = false;
+		self.done(item)
+	};
+
+	item.img.src = item.src;
+}
+
+ImageLoader.prototype.done = function(item) {
+	var self = this;
+	self.count++;
+
+	if(!item) {
+		return false;
+	}
+
+	if(!item.id) {
+		item.id = self.count;
+	}
+	
+	if(!self.isTimeout) {
+
+		//单张图片加载完成
+		self.onImageLoaded(item)
+
+		//如果所有图片加载完成，且没有超时，则清除定时器
+		if(self.count === self.images.length) {
+			clearTimeout(self.timer);
+			self.onAllImageLoaded(item);
+		}
+	}
+}
+
+//单张图片加载完成
+ImageLoader.prototype.onImageLoaded = function(item) {
+	var self = this;
+	if(!item) {
+		return false;
+	}
+
+	self.opts.onImageLoaded(item);
+}
+
+//所有图片加载完成
+ImageLoader.prototype.onAllImageLoaded = function() {
+	var self = this,
+		data = {
+		success : self.success,
+		images: self.images
+	}
+	self.opts.onAllImageLoaded(data);
+}
+
+//加载超时
+ImageLoader.prototype.onTimeout = function() {
+	var self = this;
+	self.timer = setTimeout(function() {
+		self.isTimeout = true;
+		throw new Error('超时')
+		self.opts.onTimeout();
+	}, self.opts.timeout);
+}
+
